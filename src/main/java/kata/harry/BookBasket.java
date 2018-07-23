79c3d93ff08003321b4c805cf71e0211cda2365d
HaythemZ <haythem.zayani@gmail.com>
2018-07-24 00:24:21 +0200

    simplify code

--- src/main/java/kata/harry/BookBasket.java
+++ src/main/java/kata/harry/BookBasket.java
@@ -2,15 +2,17 @@ package kata.harry;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
 
 import static java.math.BigDecimal.valueOf;
 
 public class BookBasket {
 
-    private final static BigDecimal UNITARY_BOOK_PRICE = new BigDecimal("8");
+    private final static BigDecimal UNITARY_BOOK_PRICE = BigDecimal.valueOf(8);
+    private final static BinaryOperator<BigDecimal> bigDecimalMinBinaryOperator = (p1, p2) -> p1.compareTo(p2) > 0 ? p2 : p1;
     private final List<Book> books;
     private final Discount discount;
-
 
     public BookBasket(List<Book> books) {
         this.books = books;
@@ -18,14 +20,9 @@ public class BookBasket {
     }
 
     public BigDecimal getPrice() {
-        BigDecimal bestResult = BigDecimal.ZERO;
-        for (ComputingStrategy strategy : ComputingStrategy.values()) {
-            BigDecimal priceWithStrategy = computePriceWithStrategy(strategy);
-            if (bestResult.compareTo(BigDecimal.ZERO) == 0 || bestResult.compareTo(priceWithStrategy) > 0) {
-                bestResult = priceWithStrategy;
-            }
-        }
-        return bestResult;
+        return Stream.of(ComputingStrategy.values())
+                .map(this::computePriceWithStrategy)
+                .reduce(BigDecimal.valueOf(Integer.MAX_VALUE), bigDecimalMinBinaryOperator);
     }
 
     private BigDecimal computePriceWithStrategy(ComputingStrategy strategy) {
@@ -35,14 +32,10 @@ public class BookBasket {
             Set<Book> uniqueBooks = strategy.applyStrategy(new HashSet<>(booksCopy), booksCopy);
             total = total.add(
                     UNITARY_BOOK_PRICE.multiply(valueOf(uniqueBooks.size()))
-                            .multiply(uniqueBooksRatio(uniqueBooks.size())));
+                            .multiply(discount.getDiscountFromVolumeSize(uniqueBooks.size())));
             booksCopy = removeUsedBooks(booksCopy, uniqueBooks);
         }
         return total;
-    }
-
-    private BigDecimal uniqueBooksRatio(int uniqueBooksNumber) {
-        return BigDecimal.ONE.subtract(discount.getDiscountFromVolumeSize(uniqueBooksNumber));
     }
 
     private List<Book> removeUsedBooks(List<Book> booksCopy, Set<Book> uniqueBooks) {
--- src/main/java/kata/harry/Discount.java
+++ src/main/java/kata/harry/Discount.java
@@ -8,11 +8,11 @@ public final class Discount {
 
     private final static Map<Integer, BigDecimal> DISCOUNT_TABLE = new HashMap<>();
     static {
-        DISCOUNT_TABLE.put(1, BigDecimal.ZERO);
-        DISCOUNT_TABLE.put(2, BigDecimal.valueOf(0.05));
-        DISCOUNT_TABLE.put(3, BigDecimal.valueOf(0.1));
-        DISCOUNT_TABLE.put(4, BigDecimal.valueOf(0.2));
-        DISCOUNT_TABLE.put(5, BigDecimal.valueOf(0.25));
+        DISCOUNT_TABLE.put(1, BigDecimal.ONE);
+        DISCOUNT_TABLE.put(2, BigDecimal.valueOf(0.95));
+        DISCOUNT_TABLE.put(3, BigDecimal.valueOf(0.90));
+        DISCOUNT_TABLE.put(4, BigDecimal.valueOf(0.80));
+        DISCOUNT_TABLE.put(5, BigDecimal.valueOf(0.75));
     }
 
     public BigDecimal getDiscountFromVolumeSize(int volumeSize) {
