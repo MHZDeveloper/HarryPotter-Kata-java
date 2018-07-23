66e53b298bd13def056ae61bd8a4531e28ceb144
HaythemZ <haythem.zayani@gmail.com>
2018-07-23 22:17:22 +0200

    complete simple cases

--- src/main/java/kata/harry/BookBasket.java
+++ src/main/java/kata/harry/BookBasket.java
@@ -1,18 +1,33 @@
 package kata.harry;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class BookBasket {
 
     public static final BigDecimal UNITARY_BOOK_PRICE = new BigDecimal("8");
     private final List<Book> books;
+    private final static Map<Integer, BigDecimal> DISCOUNT_TABLE = new HashMap<>();
+    static {
+        DISCOUNT_TABLE.put(1, BigDecimal.ZERO);
+        DISCOUNT_TABLE.put(2, BigDecimal.valueOf(0.05));
+        DISCOUNT_TABLE.put(3, BigDecimal.valueOf(0.1));
+        DISCOUNT_TABLE.put(4, BigDecimal.valueOf(0.2));
+        DISCOUNT_TABLE.put(5, BigDecimal.valueOf(0.25));
+    }
 
     public BookBasket(List<Book> books) {
         this.books = books;
     }
 
     public BigDecimal getPrice() {
-        return UNITARY_BOOK_PRICE;
+        HashSet<Book> books = new HashSet<>(this.books);
+        BigDecimal discount = DISCOUNT_TABLE.get(books.size());
+
+        BigDecimal ratio = BigDecimal.valueOf(1).subtract(discount);
+        return UNITARY_BOOK_PRICE.multiply(BigDecimal.valueOf(this.books.size())).multiply(ratio);
     }
 }
--- src/test/java/kata/harry/BookBasketTest.java
+++ src/test/java/kata/harry/BookBasketTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,8 +12,74 @@ public class BookBasketTest {
 
     @Test
     public void should_return_8_if_basket_contains_one_book() {
-        BookBasket bookBasket = new BookBasket(Arrays.asList(Book.VOLUME_1));
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1);
 
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
         assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(8));
     }
+
+    @Test
+    public void should_return_16_if_basket_contains_two_same_books() {
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1, Book.VOLUME_1);
+
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
+        assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(16));
+    }
+
+    @Test
+    public void should_return_15_20_if_basket_contains_two_different_books() {
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1, Book.VOLUME_2);
+
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
+        assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(15.2));
+    }
+
+    @Test
+    public void should_return_21_60_if_basket_contains_three_different_books() {
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1, Book.VOLUME_2, Book.VOLUME_3);
+
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
+        assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(21.6));
+    }
+
+    @Test
+    public void should_return_25_60_if_basket_contains_four_different_books() {
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1, Book.VOLUME_2, Book.VOLUME_3, Book.VOLUME_4);
+
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
+        assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(25.60));
+    }
+
+    @Test
+    public void should_return_30_00_if_basket_contains_five_different_books() {
+        //Given
+        List<Book> books = Arrays.asList(Book.VOLUME_1, Book.VOLUME_2, Book.VOLUME_3, Book.VOLUME_4, Book.VOLUME_5);
+
+        //When
+        BookBasket bookBasket = new BookBasket(books);
+
+        //Then
+        assertThat(bookBasket.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(30.00));
+    }
+
 }
